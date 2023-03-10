81d796139824da5ffd096dd8e21d739e1f4df4d2
Sam Salani <32249487+Samsal707@users.noreply.github.com>
2023-03-10 00:42:44 +0000

    initial commit on python course

---
+++ python_level_1_solutions/Exercises/arithematics.py
@@ -0,0 +1,13 @@
+num_1 = input('enter a number: ')
+num_2 = input('enter another number: ')
+
+total = int(num_1) + int(num_2)
+
+print('the total is: ' + str(total))
+
+
+floats_list = [1.0, 2.0, 3.0, 4.0, 5.0]
+
+average_of_floats_lists = sum(floats_list) / len(floats_list)
+
+print('average of floats list: ' + str(average_of_floats_lists))
---
+++ python_level_1_solutions/Exercises/demographic_info.py
@@ -0,0 +1,8 @@
+user_name = input("What is your name? ")
+
+user_age = input("How old are you? ")
+
+print( "Hello " + user_name + "! You are " + user_age + " years old." )
+
+
+
